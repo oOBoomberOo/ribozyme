81f91b1310a21ab57b329754d7bf1e36963e9112
oOBoomberOo <boombercmd123@gmail.com>
2020-04-25 15:38:48 +0700

    Bump commit

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,5 +1,32 @@
-use std::path::PathBuf;
+use crate::{Opt, Style};
+use crate::merger::{Merger};
+use anyhow::Result;
+use std::fs;
+use rayon::prelude::*;
 
 pub struct App {
-	directory: PathBuf
+	opt: Opt,
+}
+
+impl App {
+	pub fn new(opt: Opt) -> App {
+		App { opt }
+	}
+
+	pub fn run(&self) -> Result<()> {
+		let directory = &self.opt.directory;
+		let style = Style { pretty: self.opt.pretty };
+
+		let merger = Merger::from_path(directory)?;
+		let conflicts = merger.into_conflict_solver().solve(style)?;
+
+		let output = &self.opt.output;
+		fs::remove_dir_all(&output)?;
+		conflicts
+			.par_iter()
+			.map(|(relative, file)| (output.join(relative), file))
+			.try_for_each(|(path, file)| file.write(path))?;
+		
+		Ok(())
+	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
 use std::path::PathBuf;
 use structopt::StructOpt;
 use anyhow::Result;
-use std::collections::HashMap;
-use rayon::prelude::*;
-use std::fs;
 
 mod app;
 mod merger;
 mod rp;
 
-use merger::File;
-use merger::Merger;
+use app::App;
 
 fn main() {
 	if let Err(e) = run() {
@@ -20,26 +16,14 @@ fn main() {
 
 fn run() -> Result<()> {
 	let opt: Opt = Opt::from_args();
-	let style = Style { pretty: opt.pretty };
 
-	let merger = Merger::from_path(opt.directory)?;
-	let conflicts: HashMap<PathBuf, File> = merger
-		.into_conflict_solver()
-		.solve(style)?;
-
-	let output = opt.output;
-	fs::remove_dir_all(&output)?;
-	conflicts
-		.par_iter()
-		.map(|(relative, file)| (output.join(relative), file))
-		.try_for_each(|(path, file)| file.write(path))?;
-
-	Ok(())
+	let app = App::new(opt);
+	app.run()
 }
 
 #[derive(Debug, StructOpt)]
 #[structopt(about = "Catalyst for merging resourcepack")]
-struct Opt {
+pub struct Opt {
 	#[structopt(parse(from_str))]
 	directory: PathBuf,
 
